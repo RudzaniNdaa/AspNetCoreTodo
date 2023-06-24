14a165a01c13eafea90f6f2803de22c5aa508742
RudzaniNdaa <rudzani902@gmail.com>
2023-06-24 15:43:01 +0200

    asp.net todo app

---
+++ AspNetCoreTodo/Controllers/TodoController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace AspNetCoreTodo.Controllers
+{
+    [Route("[controller]")]
+    public class TodoController : Controller
+    {
+        private readonly ILogger<TodoController> _logger;
+
+        public TodoController(ILogger<TodoController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
